3a2a06d016b60b8bdf078651f83a275cac537122
Guillaume <gciret97@gmail.com>
2018-01-13 21:21:52 +0100

    ChronoActivity2
    Ajout de la deuxieme interface
    Il reste la gestion des notes + rendre l'interface jolie + gérer les actions inattendues de l'utilisateur

--- app/src/main/java/com/pts3/sport/ChronoActivity2.java
+++ app/src/main/java/com/pts3/sport/ChronoActivity2.java
@@ -2,43 +2,43 @@ package com.pts3.sport;
 
 
 
-        import android.content.Context;
-        import android.content.Intent;
-        import android.content.SharedPreferences;
-        import android.os.Handler;
-        import android.preference.PreferenceManager;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.ArrayAdapter;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.RadioGroup;
-        import android.widget.TableRow;
-        import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-        import com.pts3.sport.dao.Eleve;
-        import com.pts3.sport.database.ClasseManager;
-        import com.pts3.sport.database.EleveManager;
+import com.pts3.sport.dao.Eleve;
+import com.pts3.sport.database.ClasseManager;
+import com.pts3.sport.database.EleveManager;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ChronoActivity extends AppCompatActivity {
+public class ChronoActivity2 extends AppCompatActivity {
 
     private static final String ITERATOR_VALUE = "a";
     public static TextView txtValue;
     public static TextView txtAffichage;
-    public Button start, lap, stop, valider,suivant;
+    public Button start, lap, stop, valider, suivant;
 
-    ArrayList<TextView> textViewList,textViews2List;
+    ArrayList<TextView> textViewList, textViews2List;
     ArrayList<EditText> editTextList;
 
 
-    TextView temps1,temps2,temps3,temps4,eleve1,eleve2,eleve3,eleve4;
-    EditText txtInput1,txtInput2,txtInput3,txtInput4;
-    int iterator,iterator2;
+    TextView temps1, temps2, temps3, temps4, eleve1, eleve2, eleve3, eleve4;
+    EditText txtInput1, txtInput2, txtInput3, txtInput4;
+    int iterator, iterator2;
     private Chronometre chrono;
     private SharedPreferences preferences;
     private String classe;
@@ -47,36 +47,33 @@ public class ChronoActivity extends AppCompatActivity {
     private List<Eleve> listEleve;
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chrono);
+        setContentView(R.layout.activity_chrono2);
         eleve1 = findViewById(R.id.eleve1);
         eleve2 = findViewById(R.id.eleve2);
         eleve3 = findViewById(R.id.eleve3);
         eleve4 = findViewById(R.id.eleve4);
         start = (Button) findViewById(R.id.button);
-        txtAffichage = (TextView) findViewById(R.id.txtAffi);
-        txtValue = (TextView) findViewById(R.id.txtValue);
-        suivant=findViewById(R.id.btnSuivant);
+        txtAffichage = (TextView) findViewById(R.id.txtAffi2);
+        txtValue = (TextView) findViewById(R.id.txtValue2);
+        suivant = findViewById(R.id.btnSuivant);
         lap = (Button) findViewById(R.id.buttonLap);
-        stop = (Button)findViewById(R.id.btnStop);
-        valider = (Button)findViewById(R.id.btnValider);
-        Intent intent=getIntent();
-        iterator = intent.getIntExtra(ITERATOR_VALUE,0);
+        stop = (Button) findViewById(R.id.btnStop);
+        valider = (Button) findViewById(R.id.btnValider);
+        Intent intent = getIntent();
+        iterator = intent.getIntExtra(ITERATOR_VALUE, 0);
         chrono = new Chronometre(this, new Handler());
-        String[] nom = {"Théo", "Marceau", "Guillaume","Test"}; //mettre les eleves a noter (classe)
+
 
         textViewList = new ArrayList<>();
         editTextList = new ArrayList<>();
 
 
-        txtInput1 =  findViewById(R.id.txtInput);
-        txtInput2 =  findViewById(R.id.txtInput2);
-        txtInput3 =  findViewById(R.id.txtInput3);
+        txtInput1 = findViewById(R.id.txtInput);
+        txtInput2 = findViewById(R.id.txtInput2);
+        txtInput3 = findViewById(R.id.txtInput3);
         txtInput4 = findViewById(R.id.txtInput4);
         temps1 = findViewById(R.id.temps1);
         temps2 = findViewById(R.id.temps2);
@@ -95,13 +92,11 @@ public class ChronoActivity extends AppCompatActivity {
         textViews2List.add(eleve2);
         textViews2List.add(eleve3);
         textViews2List.add(eleve4);
-        Log.i("iterateur",""+iterator);
-
+        Log.i("iterateur", "" + iterator);
 
 
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         classe = preferences.getString("classe", "");
-
 
 
         EleveManager eleveManager = new EleveManager(this);
@@ -109,9 +104,9 @@ public class ChronoActivity extends AppCompatActivity {
         listEleve = eleveManager.recupererTout(classeManager.recuperer(classe));
 
 
-        for(Eleve eleve : listEleve){
+        for (Eleve eleve : listEleve) {
 
-            if(iterator <= iterator2 && iterator2 <iterator +4){
+            if (iterator <= iterator2 && iterator2 < iterator + 4) {
 
 
                 textViews2List.get(iterator2 % 4).setText(eleve.getNom());
@@ -135,7 +130,7 @@ public class ChronoActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 chrono.affTemps();
-                //associer un numero
+
 
             }
         });
@@ -150,15 +145,15 @@ public class ChronoActivity extends AppCompatActivity {
         valider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for(int i=0;i<=4;i++){
+                for (int i = 0; i <= 4; i++) {
                     int startL = txtAffichage.getLayout().getLineStart(i);
-                    int endL= txtAffichage.getLayout().getLineEnd(i);
-                    String getTextOnLine = (String) txtAffichage.getText().subSequence(startL,endL);
-                    Log.i("temps",getTextOnLine);
-                    int j=0;
-                    for(EditText eT : editTextList){
+                    int endL = txtAffichage.getLayout().getLineEnd(i);
+                    String getTextOnLine = (String) txtAffichage.getText().subSequence(startL, endL);
+                    Log.i("temps", getTextOnLine);
+                    int j = 0;
+                    for (EditText eT : editTextList) {
 
-                        if(String.valueOf(eT.getText()).equals(""+i)){
+                        if (String.valueOf(eT.getText()).equals("" + i)) {
                             Log.i("numéro", String.valueOf(eT.getText()));
                             textViewList.get(j).setText(getTextOnLine);
                         }
@@ -172,8 +167,8 @@ public class ChronoActivity extends AppCompatActivity {
         suivant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ChronoActivity.this,ChronoActivity2.class);
-                  intent.putExtra(ITERATOR_VALUE,iterator);
+                Intent intent = new Intent(ChronoActivity2.this, ChronoActivity.class);
+                intent.putExtra(ITERATOR_VALUE, iterator + 4);
                 startActivity(intent);
 
 
@@ -181,5 +176,4 @@ public class ChronoActivity extends AppCompatActivity {
         });
 
     }
-
 }
